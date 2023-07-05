5dd868754eb42d76f9488258a10fbad22cd33048
zahrawm <adamfatima2557@gmail.com>
2023-07-04 18:47:28 -0700

    Update bmi_calculator_page.dart

--- lib/pages/bmi_calculator_page.dart
+++ lib/pages/bmi_calculator_page.dart
@@ -51,132 +51,134 @@ class _BMICalculatorPageState extends State<BMICalculatorPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Container(
-              height: 100,
-              color: Colors.grey[100],
-              child: Column(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 100,
+                color: Colors.grey[100],
+                child: Column(
+                  children: [
+                    const Text("Weight(kg)"),
+                    Text(
+                      "${_weight.toStringAsFixed(2)} kg",
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w500, fontSize: 24),
+                    ),
+                    Slider(
+                      onChanged: (value) {
+                        _weight = value;
+                        setState(() {});
+                      },
+                      value: _weight,
+                      max: 150,
+                      min: 1,
+                    ),
+                  ],
+                ),
+              ),
+              TextField(
+                controller: heightController,
+                decoration: const InputDecoration(labelText: "Height (cm)"),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text("Weight(kg)"),
-                  Text(
-                    "${_weight.toStringAsFixed(2)} kg",
-                    style: const TextStyle(
-                        fontWeight: FontWeight.w500, fontSize: 24),
-                  ),
-                  Slider(
-                    onChanged: (value) {
-                      _weight = value;
+                  InkWell(
+                    onTap: () {
+                      gender = Gender.male;
                       setState(() {});
                     },
-                    value: _weight,
-                    max: 150,
-                    min: 1,
+                    child: Card(
+                      color: gender == Gender.male
+                          ? Colors.blue[100]
+                          : Colors.grey.shade50,
+                      child: SizedBox(
+                        height: 150,
+                        width: 150,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Male",
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            ),
+                            Icon(Icons.check_circle,
+                                size: 30,
+                                color: gender == Gender.male
+                                    ? Colors.green
+                                    : Colors.green.shade300)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      gender = Gender.female;
+                      setState(() {});
+                    },
+                    child: Card(
+                      color: gender == Gender.female
+                          ? Colors.pink[100]
+                          : Colors.grey.shade50,
+                      child: SizedBox(
+                        height: 150,
+                        width: 150,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Female",
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            ),
+                            Icon(Icons.check_circle,
+                                size: 30,
+                                color: gender == Gender.female
+                                    ? Colors.green
+                                    : Colors.green.shade300)
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
-            ),
-            TextField(
-              controller: heightController,
-              decoration: const InputDecoration(labelText: "Height (cm)"),
-              keyboardType: TextInputType.number,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  onTap: () {
-                    gender = Gender.male;
-                    setState(() {});
-                  },
-                  child: Card(
-                    color: gender == Gender.male
-                        ? Colors.blue[100]
-                        : Colors.grey.shade50,
-                    child: SizedBox(
-                      height: 150,
-                      width: 150,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            "Male",
-                            style: TextStyle(
-                                fontSize: 25, fontWeight: FontWeight.bold),
-                          ),
-                          Icon(Icons.check_circle,
-                              size: 30,
-                              color: gender == Gender.male
-                                  ? Colors.green
-                                  : Colors.green.shade300)
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    gender = Gender.female;
-                    setState(() {});
-                  },
-                  child: Card(
-                    color: gender == Gender.female
-                        ? Colors.pink[100]
-                        : Colors.grey.shade50,
-                    child: SizedBox(
-                      height: 150,
-                      width: 150,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            "Female",
-                            style: TextStyle(
-                                fontSize: 25, fontWeight: FontWeight.bold),
-                          ),
-                          Icon(Icons.check_circle,
-                              size: 30,
-                              color: gender == Gender.female
-                                  ? Colors.green
-                                  : Colors.green.shade300)
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            MaterialButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              onPressed: () {
-                calculateBMI();
-              },
-              height: 60,
-              minWidth: double.infinity,
-              color: const Color(0xFF322751),
-              child: const Text(
-                "Calculate BMI",
-                style: TextStyle(color: Colors.white),
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Your BMI IS : ${bmiValue.toStringAsFixed(2)}\n $message",
-              style: const TextStyle(
-                  color: Color(0xFF322751),
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold),
-            )
-          ],
+              MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                onPressed: () {
+                  calculateBMI();
+                },
+                height: 60,
+                minWidth: double.infinity,
+                color: const Color(0xFF322751),
+                child: const Text(
+                  "Calculate BMI",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Your BMI IS : ${bmiValue.toStringAsFixed(2)}\n $message",
+                style: const TextStyle(
+                    color: Color(0xFF322751),
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
         ),
       ),
     );
